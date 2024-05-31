91821e6a75cd915ffb10251dbcd90032eaf08f93
GatoMestre420 <luancurt@outlook.com>
2024-05-31 16:40:00 -0300

    moving pieces

---
+++ src/chess/ChessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+import boardgame.BoardException;
+
+public class ChessException extends BoardException{
+    private static final long serialVerionUID = 1L;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+}
